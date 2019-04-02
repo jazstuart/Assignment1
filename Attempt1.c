8e72f0fd9fb4eeb21c8c5cedab61f569902909a5
Jasmine Stuart <jasminecstuart@gmil.com>
2019-04-02 02:35:17 +0000

    Encoding individual characters of a message (confusing but it works)

--- Attempt1.c
+++ Attempt1.c
@@ -3,13 +3,34 @@
 int main()
 {
     char message[100] = "TEST CODE";
-    int index=0;
+    int index = 0;
     int value;
+    int key = 3;
     
-    while (index < 10) 
+    while (index < 9) 
     {
-        value = message[index];
-        printf("%c has an ASCII value of %d\n", message[index], value);
+        char x = message[index];
+        value = x;
+        printf("%c has an ASCII value of %d\n", x, value);
+        
+        x = x - key;
+        
+        if (x + key == 32) 
+        {
+            x = x + key;
+        }
+        else if (x >= 65 && x <= 90)
+        {
+            x = x;
+        }
+        else
+        {
+            x = x + 26;
+        }
+        
+        
+        printf("Encoded character is %c\n", x);
+
         index++;
     }
     
