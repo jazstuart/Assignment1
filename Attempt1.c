b46686bf3b2669357d8d984f90377e756f20e721
Jasmine Stuart <jasminecstuart@gmil.com>
2019-04-02 01:23:28 +0000

    second commit

--- Attempt1.c
+++ Attempt1.c
@@ -1,8 +1,29 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//    const char message[26] = "hello";
+//    char c;
+//    
+//    scanf("%c", &c);
+//    printf("%c\n", c);
+//    
+//    printf("%c\n", message[0]);
+//    printf("%s\n", message);
+//    
+//    return 0;
+//}
 #include <stdio.h>
-
-
-
 int main()
 {
+    char c;
+    printf("Enter a character: ");
+
+    // Reads character input from the user
+    scanf("%c", &c);  
     
+    // %d displays the integer value of a character
+    // %c displays the actual character
+    printf("ASCII value of %c = %d", c, c);
+    return 0;
 }
