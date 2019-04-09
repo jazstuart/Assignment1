746abf833c1660c1a36f6d12b2e500bd4df2b8cc
Jasmine Stuart <jasminecstuart@gmil.com>
2019-04-09 03:56:04 +0000

    Function encryptRotation( ) for encoding a message using a rotation cipher, user menu

--- Attempt1.c
+++ Attempt1.c
@@ -1,53 +1,95 @@
 #include <stdio.h>
 #include <string.h>
 
-//char encryptRotation(char message, int key);
+void encryptRotation(char *message, int key);
 
 int main()
 {
-//    char c=1;
-//    char message[100];
-//    
-//    printf("Please select from the following options by entering the corresponding number (1-6):\n");
-//    printf("1. Encryption with a rotation cipher, known key\n");
-//    printf("2. Decryption of a rotation cipher, known key\n");
-//    printf("3. Encryption with a substituation cipher, known key\n");
-//    printf("3. Decryption of a substituation cipher, known key\n");
-//    printf("3. Decryption of a rotation cipher, unknown key\n");
-//    printf("3. Decryption of a substituation cipher, unknown key\n");
-//    
-//    //scanf("%c", &c);
-//    
-//    if (c == 1 || c == 3) {
-//        printf("Enter message to encode: ");
-//        printf("%s", message);
-//    }
-//    else {
-//        printf("Enter message to decode: ");
-//        scanf("%s", message);
-//    }
-//
-//    switch(c) {
-//        case 1: encryptRotation(message, c);
-//            break;
-////        case 2: 
-////            break;
-////        case 3:
-////            break;
-////        case 4:
-////            break;
-////        case 5:
-////            break;
-////        case 6:
-////            break;
-//        default: return 0;
-//    }
-
+    int c=1;
+    char message[10] = "TEST";
     
-    char message[100] = "JASMINE";
+    printf("Please select from the following options by entering the corresponding number (1-6):\n");
+    printf("1. Encryption with a rotation cipher, known key\n");
+    printf("2. Decryption of a rotation cipher, known key\n");
+    printf("3. Encryption with a substituation cipher, known key\n");
+    printf("3. Decryption of a substituation cipher, known key\n");
+    printf("3. Decryption of a rotation cipher, unknown key\n");
+    printf("3. Decryption of a substituation cipher, unknown key\n");
+    
+    //scanf("%c", &c);
+    
+    if (c == 1 || c == 3) {
+        printf("Enter message to encode: ");
+        scanf(" %[^\n]s", message);
+    }
+    else {
+        printf("Enter message to decode: ");
+        scanf(" %[^\n]s", message);
+    }
+
+    switch(c) {
+        case 1: encryptRotation(message, c);
+        printf("%s\n", message);
+            break;
+//        case 2: 
+//            break;
+//        case 3:
+//            break;
+//        case 4:
+//            break;
+//        case 5:
+//            break;
+//        case 6:
+//            break;
+        default: return 0;
+    }
+}
+    
+//    char message[100] = "JASMINE";
+//    int index = 0;
+//    int key = 4;
+//    int length;
+//    
+//    length = strlen(message);
+//    
+//    printf("Message to convert to code: %s\n", message);
+//    printf("Encoded message: ");
+//    while (index < length)
+//    {
+//        char x = message[index];
+//        
+//        x = x - key;
+//        
+//        if (x + key == 32) 
+//        {
+//            x = x + key;
+//        }
+//        else if (x >= 65 && x <= 90)
+//        {
+//            x = x;
+//        }
+//        else
+//        {
+//            x = x + 26;
+//        }
+//        
+//        printf("%c", x);
+//
+//        index++;
+//    }
+//    
+//    
+//    return 0;
+//}
+
+
+
+
+void encryptRotation(char *message, int key) {
+    
     int index = 0;
-    int key = 4;
     int length;
+    
     
     length = strlen(message);
     
@@ -56,7 +98,6 @@ int main()
     while (index < length)
     {
         char x = message[index];
-        
         x = x - key;
         
         if (x + key == 32) 
@@ -72,18 +113,11 @@ int main()
             x = x + 26;
         }
         
-        printf("%c", x);
+        message[index] = x;
 
         index++;
     }
     
+    //return x;
     
-    return 0;
 }
-
-
-
-
-//char encryptRotation(char message, int key) {
-//    
-//}
