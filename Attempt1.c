b1abcdd514dcb37b72a3b1993c8d591808a93c35
Jasmine Stuart <jasminecstuart@gmil.com>
2019-04-10 05:02:15 +0000

    Added rotation cipher decryption, using same method as encryption

--- Attempt1.c
+++ Attempt1.c
@@ -2,11 +2,13 @@
 #include <string.h>
 
 void encryptRotation(char *message, int key);
+void decryptRotation(char *message, int key);
 
 int main()
 {
-    int c=1;
-    char message[10] = "TEST";
+    int c;
+    char message[100];
+    int k;
     
     printf("Please select from the following options by entering the corresponding number (1-6):\n");
     printf("1. Encryption with a rotation cipher, known key\n");
@@ -16,7 +18,8 @@ int main()
     printf("3. Decryption of a rotation cipher, unknown key\n");
     printf("3. Decryption of a substituation cipher, unknown key\n");
     
-    //scanf("%c", &c);
+    printf("Selection: ");
+    scanf("%d", &c);
     
     if (c == 1 || c == 3) {
         printf("Enter message to encode: ");
@@ -26,13 +29,17 @@ int main()
         printf("Enter message to decode: ");
         scanf(" %[^\n]s", message);
     }
+    
+    printf("Enter key: ");
+    scanf("%d", &k);
 
     switch(c) {
-        case 1: encryptRotation(message, c);
-        printf("%s\n", message);
+        case 1: encryptRotation(message, k);
+            printf("%s\n", message);
             break;
-//        case 2: 
-//            break;
+        case 2: decryptRotation(message, k);
+            printf("%s\n", message);
+            break;
 //        case 3:
 //            break;
 //        case 4:
@@ -90,10 +97,8 @@ void encryptRotation(char *message, int key) {
     int index = 0;
     int length;
     
-    
     length = strlen(message);
     
-    printf("Message to convert to code: %s\n", message);
     printf("Encoded message: ");
     while (index < length)
     {
@@ -118,6 +123,39 @@ void encryptRotation(char *message, int key) {
         index++;
     }
     
-    //return x;
+}
+
+
+void decryptRotation(char *message, int key) {
+        
+    int index = 0;
+    int length;
+    
+    length = strlen(message);
+    
+    printf("Decoded message: ");
+    while (index < length)
+    {
+        char x = message[index];
+        x = x + key;
+        
+        if (x - key == 32) 
+        {
+            x = x - key;
+        }
+        else if (x >= 65 && x <= 90)
+        {
+            x = x;
+        }
+        else
+        {
+            x = x - 26;
+        }
+        
+        message[index] = x;
+
+        index++;
+    }
     
 }
+
